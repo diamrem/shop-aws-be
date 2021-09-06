34e076a7fef4824086c4c44210eecd60ed802b3a
SeLub <siteup24@gmail.com>
2021-09-07 01:13:43 +0300

    feat: getProductsList get from DB

--- product-service/functions/getProductsList/getProductsList.js
+++ product-service/functions/getProductsList/getProductsList.js
@@ -1,5 +1,6 @@
 
-import products from '../productList.json';
+import { Client } from 'pg';
+//const { Client } = require("pg");
 
 const handleResponse = (products = {}, status = 200) => ({
   headers: {
@@ -11,4 +12,26 @@ const handleResponse = (products = {}, status = 200) => ({
   body: JSON.stringify(products),
 });
 
-export const handler = async event => await handleResponse(products);
+const credentials = {
+  user: "postgres",
+  host: "task-4.citzxb6pxpgh.eu-central-1.rds.amazonaws.com",
+  database: "productList",
+  password: "wD2i3pq31Ou7mqNQO5e4",
+  port: 5432,
+};
+
+async function getProductList() {
+  const client = new Client(credentials);
+  await client.connect();
+  const rows = await client.query('select * from products');
+  await client.end();
+
+  return rows;
+}
+
+//(async () => {
+//  const clientResult = await getProductList();
+//  console.log("ProductList: " + JSON.stringify(handleResponse(clientResult)));
+//})();
+
+export const handler = async event => await handleResponse(await getProductList());
