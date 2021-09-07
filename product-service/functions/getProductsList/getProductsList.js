bfc252dc799b4bff40d43380a4c3238237bee436
SeLub <siteup24@gmail.com>
2021-09-07 10:54:15 +0300

    feat: add .env

--- product-service/functions/getProductsList/getProductsList.js
+++ product-service/functions/getProductsList/getProductsList.js
@@ -11,6 +11,20 @@ const handleResponse = (products = {}, status = 200) => ({
   body: JSON.stringify(products),
 });
 
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const credentials = {
+  user: PG_USERNAME,
+  host: PG_HOST,
+  database: PG_DATABASE,
+  password: PG_PASSWORD,
+  port: PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
+
 const credentials = {
 
 };
